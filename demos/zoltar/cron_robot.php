d0db2677aad3588a455b4fa5e07269523600da1c
Erika Focke <erika.christina@gmail.com>
2011-07-21 08:04:18 -0300

    New 'demos' folder

---
+++ demos/zoltar/cron_robot.php
@@ -0,0 +1,81 @@
+<?php
+/* mais info: http://twittertools.in/2011/07/21/novo-demo-oraculo-de-zoltar/ */
+/* incluir bibliotecas. atualize com o caminho onde você as salvou. */
+require_once("../v3/lib/TwitterTools.php");
+require_once("../v3/lib/TwitterOAuth.php");
+require_once("../v3/lib/OAuth.php");
+
+/* consumer key & consumer secret*/
+$consumer_key = "your_consumer_key";
+$consumer_secret = "your_consumer_secret";
+
+/* access token & access token secret from your twitter user */
+$access_token = "your_atoken";
+$access_token_secret = "your_atoken_secret";
+
+$tw = new TwitterTools($consumer_key,$consumer_secret,$access_token,$access_token_secret);
+
+/* possible answers */
+
+$answers[] = "Isso será realizado.";
+$answers[] = "Melhor esquecer.";
+$answers[] = "Quem sabe outro dia?";
+$answers[] = "A resposta está dentro de você.";
+$answers[] = "Você já sabe a resposta.";
+$answers[] = "Isso está fora do meu alcance.";
+$answers[] = "Certamente.";
+$answers[] = "Isso é um mistério. O futuro é uma caixinha de surpresas.";
+$answers[] = "Os astros dizem que sim.";
+$answers[] = "Isso é possível, só depende de você.";
+$answers[] = "Está escrito.";
+$answers[] = "Tudo é possível.";
+
+
+$last_mention_id = 0;
+if(is_file("lastmention.id"))
+	$last_mention_id = file_get_contents("lastmention.id");
+
+// obter replies e responder
+
+$params['count'] = 10;
+if($last_mention_id)
+	$params['since_id'] = $last_mention_id;
+
+//get only new mentions if they exist
+$result = json_decode($tw->makeRequest('http://api.twitter.com/1/statuses/mentions.json',$params),1);
+
+if($result)
+{
+	/*debug*/
+	/* echo "<pre>";
+	print_r($result);
+	echo "</pre>";*/
+	foreach($result as $tweet)
+	{
+		if(!$lastid)
+			$lastid = $tweet['id_str'];
+		
+		//evita responder duplicadamente
+		if(!in_array($tweet['user']['screen_name'],$replies))
+		{			
+			//pergunta?
+			$pos = strpos($tweet['text'],'?');
+			if($pos !== false)
+			{
+				//responde pergunta
+				$rand = array_rand($answers);
+				$tw->update("@".$tweet['user']['screen_name']." ".$answers[$rand],$tweet['id_str']);
+				$replies[] = $tweet['user']['screen_name'];
+			}
+		}
+	}
+	
+	$fp = fopen("lastmention.id",'w');				
+	fwrite($fp,$lastid);
+	fclose($fp);
+
+}
+else
+	echo "Não há mentions para responder.";
+?>
+
---
+++ demos/zoltar/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TwitterTools demo: ZOLTAR</title>
+<style>
+body {
+	background-color: #000;
+	font-family: arial;
+	font-size: 12px;
+	color: #f5f5f5;
+}
+
+a, a:visited {
+	color: #FFF;
+}
+</style>
+</head>
+<body>
+<img src="greatZoltar.gif" style="float:left;margin-right:10px;"/><h2>The Great Zoltar</h2>
+
+<p>Este é um projeto de demonstração da biblioteca TwitterTools. "Zoltar" é um robô de resposta automática no Twitter, que responde os usuários com predições sobre o futuro.</p>
+<p>Para testar, basta enviar uma pergunta para o perfil @GreatZoltar. Em pouco tempo ele responderá.</p>
+<p>O tutorial completo e o código para download estão disponíveis aqui: <a href="http://twittertools.in/2011/07/21/novo-demo-oraculo-de-zoltar/">http://twittertools.in/2011/07/21/novo-demo-oraculo-de-zoltar/</a></p>
+<br clear="all"/>
+
+</body>
+</html>
