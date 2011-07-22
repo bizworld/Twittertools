b0dbabb9cb283a3844961f7ed679e77914950e86
Erika Focke <erika.christina@gmail.com>
2011-07-22 10:20:22 -0300

    updated to v3.0

--- index.php
+++ index.php
@@ -1,59 +1,51 @@
-<?php
-//essencial!
-session_name("TTDemo");
-session_start();
-
-if(isset($_GET['logout']))
-	session_unset();
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>TwitterTools - DEMO</title>
-<link href="examples/style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TwitterTools - DEMOS</title>
+<style>
+body {
+	font-family: arial;
+	font-size: 12px;
+	color: #666;
+}
+
+ul.methods {
+	float: left;
+	width: 260px;
+	margin: 10px;
+	font-size: 14px;
+}
+
+ul.methods li a{
+	background-color: #E5E5E5;
+	padding: 5px;
+	text-decoration: none;
+	display: block;
+	margin: 1px;
+}
+
+ul.methods li a:hover {
+	font-weight: bold;
+	background-color: #F5F5F5;
+}
+
+div.boxRetorno {
+	color: #FFF;
+	background-color: #000;
+	padding: 10px;
+	margin: 10px;
+}
+
+</style>
 </head>
 <body>
-<h2>Twitter Tools Demo</h2>
-<?php
-require_once("lib/TwitterTools.php");
-require_once("lib/TwitterOAuth.php");
-require_once("lib/OAuth.php");
+<h2>Twitter Tools Demos - v3.0</h2>
 
-	/* consumer key & consumer secret - register an app to get yours at:
-	 * http://dev.twitter.com/apps/new
-	 */
-	$consumer_key = "lgeljXiueyOLElkT4reEwA";
-	$consumer_secret = "SkppBLCv652ycImVRnojAKwyy2rJj1gnqGgo4hBtfI";
-	
-	$tw = new TwitterTools($consumer_key,$consumer_secret);
+<ul class="methods">
+	<li><a href="playground.php">Playground - retornos dos métodos</a></li>
+	<li><a href="../zoltar/index.php">Oráculo de Zoltar (aplicação robô)</a></li>
+</ul>
 
-
-	if(!$tw->state)
-	{		
-		$request_link = $tw->getAuthLink();
-		echo '<h3>Sign in with your twitter account</h3>';
-		echo '<p><a href="'.$request_link.'" title="sign in with your twitter account"><img src="img/sign-in-with-twitter-d.png" /></a></p>';
-	} 
-	else 
-	{	
-		$credentials = $tw->getCredentials();
-		
-		?>
-		<p>You are logged in as: <strong><?=$credentials->screen_name?></strong> [ <a href="./?logout=1">LOGOUT</a> ]</p>	
-<?
-	}//else
-?>
-			<div class="box">
-			<h3>All examples / Tests</h3>
-			<p><strong>Update</strong> <a href="examples/update.php">You can update your status in this example.</a></p>
-			<p><strong>Follow</strong> <a href="examples/follow.php">Click here to check a follow example.</a></p>
-			<p><strong>Timeline</strong> <a href="examples/timeline.php">Click here to view your timelime (now with retweet option).</a></p>
-			<p><strong>Mentions</strong> <a href="examples/mentions.php">Click here to view your mentions (@'s).</a></p>
-			<p><strong>DMs</strong> <a href="examples/dms.php">Click here to view your DMs.</a></p>
-			<p><strong>Followers</strong> <a href="examples/followers.php">Click here to view your followers or any twitter user followers.</a></p>
-			<p><strong>Friends</strong> <a href="examples/friends.php">Click here to view your friends or any twitter user friends.</a></p>
-			<p><strong>Search</strong> <a href="examples/search.php">Click here to search on Twitter.</a></p>
-			</div>
 </body>
 </html>
